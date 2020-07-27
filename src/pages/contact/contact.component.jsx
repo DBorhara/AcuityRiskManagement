42789636d6f27b267957a6bb25d482d1c701ab1f
Depak Borhara <depakborhara@gmail.com>
2020-07-26 23:46:46 -0400

    updated email form with emailjs and reactstrap

--- src/pages/contact/contact.component.jsx
+++ src/pages/contact/contact.component.jsx
@@ -1,63 +1,110 @@
 import React, { Component } from 'react';
+import emailjs from 'emailjs-com';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+
 import './contact.styles.scss';
 
 import Hero from '../../components/heroimage/heroimage.component';
 
 class ContactPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+  state = {
+    name: '',
+    email: '',
+    industry: '',
+    message: '',
+  };
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    emailjs
+      .sendForm(
+        'gmail',
+        'acquityemailform',
+        event.target,
+        'user_bwLIaWZ7w6oGADJnXoOvJ'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    this.resetForm();
+  }
+  resetForm() {
+    this.setState({
       name: '',
       email: '',
       industry: '',
       message: '',
-    };
+    });
   }
 
-  handleChange = (event) => {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
-
-    this.setState({
-      [name]: value,
-    });
+  handleChange = (param, e) => {
+    this.setState({ [param]: e.target.value });
   };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-
-    this.setState({
-      disabled: true,
-    });
-  };
-
   render() {
     return (
       <div>
-        <Hero />
-        <h1>Contact Us</h1>
-        <form onSubmit={this.handleSubmit}>
-          Name:
-          <input name="name" type="text" onChange={this.handleChange} />
-          Email:
-          <input name="email" type="email" onChange={this.handleChange} />
-          Industry:
-          <input name="industry" type="text" onChange={this.handleChange} />
-          Message:
-          <textarea name="message" onChange={this.handleChange} />
-          <button
-            className="button"
-            name="submit"
-            type="submit"
-            onSubmit={this.handleSubmit}
-          >
-            Submit
-          </button>
-        </form>
+        <div>
+          <Hero />
+        </div>
+        <div>
+          <h1 className="p-heading2">Get in Touch</h1>
+          <Form onSubmit={this.handleSubmit.bind(this)}>
+            <FormGroup controlId="formBasicEmail">
+              <Label>Email</Label>
+              <Input
+                type="email"
+                name="email"
+                value={this.state.email}
+                className="w-25"
+                onChange={this.handleChange.bind(this, 'email')}
+                placeholder="name@example.com"
+              />
+            </FormGroup>
+            <FormGroup controlId="formBasicName">
+              <Label className="text-muted">Name</Label>
+              <Input
+                type="text"
+                name="name"
+                value={this.state.name}
+                className="text-primary"
+                onChange={this.handleChange.bind(this, 'name')}
+                placeholder="Name"
+              />
+            </FormGroup>
+            <FormGroup controlId="formBasicindustry">
+              <Label className="text-muted">Industry</Label>
+              <Input
+                type="text"
+                name="industry"
+                className="text-primary"
+                value={this.state.industry}
+                onChange={this.handleChange.bind(this, 'industry')}
+                placeholder="Industry"
+              />
+            </FormGroup>
+            <FormGroup controlId="formBasicMessage">
+              <Label className="text-muted">Message</Label>
+              <Input
+                type="textarea"
+                name="message"
+                className="text-primary"
+                value={this.state.message}
+                onChange={this.handleChange.bind(this, 'message')}
+              />
+            </FormGroup>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
       </div>
     );
   }
 }
-
 export default ContactPage;
