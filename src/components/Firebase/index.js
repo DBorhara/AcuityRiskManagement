cbb02c11f23529e5bfd448d5e9763ba5bff3fea8
Depak Borhara <depakborhara@gmail.com>
2020-09-22 21:09:37 -0400

    Updating endpoints to clients

--- src/components/Firebase/index.js
+++ src/components/Firebase/index.js
@@ -4,12 +4,12 @@ import 'firebase/database';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: 'acuityrisk.firebaseapp.com',
-  databaseURL: 'https://acuityrisk.firebaseio.com',
-  projectId: 'acuityrisk',
-  storageBucket: 'acuityrisk.appspot.com',
-  messagingSenderId: '773537591164',
-  appId: '1:773537591164:web:5f0b335b4c24df1a207ef9',
+  authDomain: 'acuity-website-74227.firebaseapp.com',
+  databaseURL: 'https://acuity-website-74227.firebaseio.com',
+  projectId: 'acuity-website-74227',
+  storageBucket: 'acuity-website-74227.appspot.com',
+  messagingSenderId: '411912877767',
+  appId: '1:411912877767:web:b87d308340c61216bf70e2',
 };
 
 firebase.initializeApp(firebaseConfig);
